2251fdb98260de5c3ba0597031f76476d865a6a8
gargi <gargiramacharan@berkeley.edu>
2026-04-23 21:53:49 -0700

    changed port to 8000 to avoid airplay conflict

--- server/index.js
+++ server/index.js
@@ -4,17 +4,27 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.options('/{*path}', cors(corsOptions));
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Oski Subleasing API is running');
+});
 
 app.use('/api/listings', require('./routes/listings'));
 app.use('/api/contact', require('./routes/contact'));
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected')).catch((err) => console.log(err));
-
-app.get('/', (req, res) => {
-    res.send('Oski Subleasing API is running');
-});
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
